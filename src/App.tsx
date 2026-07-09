a2734eb9c294faac3865e21314d99991c3a5d6d6
Skill Template <template@skill>
2026-07-10 06:18:11 +0800

    chore(backend-building): graft features db auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import StarNestBackground from '@/components/StarNestBackground';
 import LandingPage from '@/pages/LandingPage';
 import DashboardPage from '@/pages/DashboardPage';
+import Login from "./pages/Login"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/*" element={<LandingPage />} />
           <Route path="/dashboard/*" element={<DashboardPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </>
